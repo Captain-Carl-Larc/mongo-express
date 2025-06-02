59f60d3a2b2b75228f1dad2c251cbfbf148d7fee
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-06-02 07:13:06 +0300

    connecting to db

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Server is running")
+// Importing required modules
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//initializing express app
+const app = express();
+
+//configuring dotenv to use environment variables
+dotenv.config();
+
+
+//connecting to MongoDB
+const PORT = process.env.PORT || 3000;
+const dbUri = process.env.MONGO_URI
+
+mongoose.connect(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
